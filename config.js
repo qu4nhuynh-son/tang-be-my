516afac0289d12d0c3028a45e358d8d8dd4692c6
qu4nhuynh-son <116681639+qu4nhuynh-son@users.noreply.github.com>
2022-12-04 12:54:19 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,15 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
     introTitle: '🖐🖐🖐 Hé lô ông già của anh! 🖐🖐🖐',
-    introDesc: `🎄🎄🎄🎄Chúc em giáng sinh vui vẻ, luôn vui vẻ và hạnh phúc 🎄🎄🎄🎄, 
+    introDesc: `🎄🎄🎄🎄Chúc em giáng sinh vui vẻ, và hạnh phúc 🎄🎄🎄🎄, 
     thật ra thì anh có nhiều thứ muốn nói với em lắm
-    anh muốn nói trực tiếp bằng lời nói hơn, và anh muốn nhìn ngắm khuôn mặt của em để nói lời này, không biết em cho anh cơ hội được không ?, anh biết lỗi của mình lắm rồi, anh đã suy nghĩ rất nhiều về chuyện 2 đứa mình nhiều lắm và anh muốn hỏi là:    `,
-    btnIntro: 'OK',
-    title: 'Vậy em cho anh hỏi là, hỏi là em nè, em cho anh cơ hội làm em được hạnh phúc 1 lần nữa được không ? 😙',
+    anh muốn nói trực tiếp bằng lời nói hơn, và anh muốn nhìn ngắm khuôn mặt của em để nói lời này, không biết em cho anh cơ hội được không ?, anh biết lỗi của mình lắm rồi, anh đã suy nghĩ rất nhiều về chuyện 2 đứa mình nhiều lắm, về những gì em đã trải qua 
+    những cô đơn, những lần khóc 1 mình mà em đã phải chống chọi, khi nghe được những sự việc đó, lòng anh quặng đau hơn nữa, anh thấy mình tệ và vô tâm quá, sao anh có thể lại
+    không để ý được những điều nhỏ nhặt như vậy, bây giờ anh chỉ ước mình có thể quay ngược lại thời gian để có thể đơm thêm nhiều điều ngọt ngào, chiều chuộng em nhiều hơn
+    quan tâm chăm sóc để ý và quan trọng là có thể cho em thấy được anh không có vô tâm, không lãng mạn như em nghĩ, anh yêu em bằng cả sự chân thành với những điều nhỏ nhặt
+     nhất em nói, với anh anh điều lưu ý, và nhớ hết, anh ước gì mình có thể sửa sai và không có tỏ ra vụng về trong việc thổ lộ tình cảm của mình. Và em ơi, anh muốn hỏi là:    `,
+    btnIntro: 'Hỏi cái gì đó',
+    title: 'Vậy em cho anh hỏi là, hỏi là em nè, em cho anh cơ hội sửa sai và làm em được hạnh phúc 1 lần nữa được không, được quay lại vị trí xuất phát 1 lần nữa được không ? 😙',
     desc: 'Em không trả lời hoặc thoát ra, tức là Em chịu đó nha, ông già :> ',
     btnYes: 'Thôi được rồi, tui cho ông cơ hội đó nhưng mà phải thương tui gấp 1000000 lần nhe 💖💖💖',
     btnNo: '💔 Anh mơ à ! 💔 ',
